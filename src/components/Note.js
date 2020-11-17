bc053734e4d8ec8d7a3bf906c1ec128fb640dd8a
Anna Almestål <51927066+anna-ip@users.noreply.github.com>
2020-11-17 11:19:01 +0100

    Add files via upload

---
+++ src/components/Note.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Note = () => {
+	return (
+		<div className='note'>
+			<h1>My first note</h1>
+			<p>Building a keeper app</p>
+		</div>
+	)
+}
+
+export default Note
