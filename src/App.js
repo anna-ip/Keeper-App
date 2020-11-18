3a666bceaa2183fd500dfa839e53f3717a3c9273
anna-ip <anna.almestal@gmail.com>
2020-11-18 09:22:14 +0100

    addad some content to the notes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react'
 import Header from './components/Header'
 import Note from './components/Note'
 import Footer from './components/Footer'
+import notes from './notes'
 
 const App = () => {
-	return (
-		<>
-			<Header />
-			<Note />
-			<Footer />
-		</>
-	)
+  return (
+    <>
+      <Header />
+      {notes.map((note) => (
+        <Note key={note.key} title={note.title} content={note.content} />
+      ))}
+      <Footer />
+    </>
+  )
 }
 
 export default App
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Note = () => {
-	return (
-		<div className='note'>
-			<h1>My first note</h1>
-			<p>Building a keeper app</p>
-		</div>
-	)
+const Note = (props) => {
+  return (
+    <div className='note'>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+    </div>
+  )
 }
 
 export default Note
